aa113f51bb4505cf31a5197a2750056581682142
ASPP Student <student@localhost>
2019-09-03 09:51:26 +0200

    add some test cases

--- hands_on/local_maxima/local_maxima.py
+++ hands_on/local_maxima/local_maxima.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def local_maxima(numlist):
+    #numlist = input('enter list')
     loc = 0
     curr = numlist[0]
     for idx, elem in enumerate(numlist):
@@ -11,3 +12,5 @@ def local_maxima(numlist):
     return loc
 
 #local_maxima([1, 3, -1, 2, 1])
+#local_maxima([1,4,-5,0,2,1])
+#local_maxima([-1,-1,0,1])
